35d8be8c12c36567fd09f206747d54a4d95f4142
alexzsh <zjutzsh@gmail.com>
2018-11-25 21:36:17 +0800

    :memo: update

---
+++ leetcode/53_Maxinum_Subarray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+using namespace std;
+
+int n,m;
+string s,ss;
+int maxSubArray(vector<int>& nums) {
+    int len = nums.size();
+    int Max = nums[0];
+    for(int i=1;i<len;++i){
+        nums[i]=max(nums[i],nums[i]+nums[i-1]);
+        Max = max(Max,nums[i]);
+    }
+    return Max;
+}
+int main() {
+
+}
+
