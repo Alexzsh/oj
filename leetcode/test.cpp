c4d05910924f15e313d401a75ae1a6d716a714a5
alexzsh <zjutzsh@gmail.com>
2019-01-01 20:28:00 +0800

    update

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -1,23 +1,61 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
-#include<cstring>
-#include<cmath>
 using namespace std;
+string strc(string a,int b)
+{
+	string r;
+	for(int i = 0;i<b;i++)
+		r+=a;
+	return r;
+}
+string one,two,three,four,five,Yi_Zu_Shu;
 
-int n,m;
-string s,ss;
-int a[100]={0};
-void test(int &b,int p[]){
-    b=10;
-    cout<<p[0];
-    p[1]=1;
-    cout<<p[1];
+int main(){
+	int k;
+	cin>>k;
+	cin>>Yi_Zu_Shu;
+	for(int i = 0;i<Yi_Zu_Shu.length();i++)
+	{
+		if(Yi_Zu_Shu[i]== '1' || Yi_Zu_Shu[i]== '4')
+			//one+="";
+			one+="  "+strc(" ",k)+" ";
+		else
+			one+="  "+strc("-",k)+" ";//one+="  "+strc("-",k)+" ";
+		if(Yi_Zu_Shu[i]== '1' || Yi_Zu_Shu[i]== '2' || Yi_Zu_Shu[i]== '3' || Yi_Zu_Shu[i]== '7')
+			//two+="| ";
+			two+="  "+strc(" ",k)+"|";
+		else if(Yi_Zu_Shu[i]== '5' || Yi_Zu_Shu[i]== '6')
+			two+=" |"+strc(" ",k)+" ";
+		else
+			//two+="|| ";
+			two+=" |"+strc(" ",k)+"|";
+		if(Yi_Zu_Shu[i]== '1' || Yi_Zu_Shu[i]== '7' || Yi_Zu_Shu[i]== '0')
+			//three+="";
+			three+="  "+strc(" ",k)+" ";
+		else
+			//three+=strc("-",k)+" ";
+			three+="  "+strc("-",k)+" ";
+		if(Yi_Zu_Shu[i] == '2')
+			//four+="| ";
+			four+=" |"+strc(" ",k)+" ";
+		else if(Yi_Zu_Shu[i] == '6' || Yi_Zu_Shu[i] == '8' || Yi_Zu_Shu[i] == '0')
+			//four+="|| ";
+			four+=" |"+strc(" ",k)+"|";
+		else
+			//four+="| ";
+			four+="  "+strc(" ",k)+"|";
+		if(Yi_Zu_Shu[i] == '1' || Yi_Zu_Shu[i] == '4' || Yi_Zu_Shu[i] == '7')
+			//five+="";
+			five+="  "+strc(" ",k)+" ";
+		else
+			//five+=strc("-",k)+" ";
+			five+="  "+strc("-",k)+" ";
+	}
+	cout<<one<<"\n";
+	for(int i = 0;i<k;i++)cout<<two<<"\n";
+	cout<<three<<"\n";
+	for(int i = 0;i<k;i++)cout<<four<<"\n";
+	cout<<five<<"\n";
+	
 }
-int main() {
-    int aa=1;
-    test(aa,a);
-    cout<<a[0]<<" "<<a[1];
-    cout<<aa;
-}
+
 
