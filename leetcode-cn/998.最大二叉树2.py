fce0a5a00d6b0269a3ee9408b785d6a0f1124639
alexzsh <zjutzsh@gmail.com>
2019-03-30 17:13:52 +0800

    updatedaily

---
+++ leetcode-cn/998.最大二叉树2.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+#author: zsh
+#modified by zsh at date
+#version:
+#history:
+#relate on:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def insertIntoMaxTree(self, root: TreeNode, val: int) -> TreeNode:
+        if root==None:
+            return TreeNode(val)
+        if root.val<val:
+            node = TreeNode(val)
+            node.left=root
+            return node
+        
+        root.right=self.insertIntoMaxTree(root.right,val) 
+        return root
+        
