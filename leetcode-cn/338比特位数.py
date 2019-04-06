d673db7d4d76437f2f69fd76f727c72c76512efa
alexzsh <zjutzsh@gmail.com>
2019-04-06 14:19:43 +0800

    updatedaily

---
+++ leetcode-cn/338比特位数.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+#author: zsh
+#modified by zsh at date
+#version:
+#history:
+#relate on:
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res=[0]*(num+1)
+        for i in range(1,num+1):
+            res[i]=res[i&(i-1)]+1
+        return res
